0f30101799e456705fb779d63027015014ecfcbe
Bojuuh <mbjerrumj@gmail.com>
2025-11-04 13:42:45 +0100

    Done 2

---
+++ README.md
@@ -0,0 +1,9 @@
+## Exam REST Exercise
+
+### How to run
+1. Create a database in your local postgres instance called 'exam'
+2. Run the main method in config.Populator class to populate the database with some data
+3. Run the main method in the Main class to start the server on port 7070
+4. See the routes in your browser at 'http://localhost:7070/routes'
+5. Request the 'http://localhost:7070/candidates' endpoint in your browser to see the list of candidates their skills
+6. Use the dev.http file to test the available routes
--- src/main/java/app/entities/Candidate.java
+++ src/main/java/app/entities/Candidate.java
@@ -1,5 +1,4 @@
 package app.entities;
-import app.dtos.CandidateDTO;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -25,30 +24,4 @@ public class Candidate {
     @OneToMany(mappedBy = "candidate", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<CandidateSkill> candidateSkills;
 
-    public void addSkill(Skill skill) {
-        if (skill == null) return;
-        boolean exists = candidateSkills.stream()
-                .anyMatch(cs -> cs.getSkill().equals(skill));
-        if (exists) return;
-
-        CandidateSkill cs = CandidateSkill.builder()
-                .candidate(this)
-                .skill(skill)
-                .build();
-        candidateSkills.add(cs);
-        skill.getCandidateSkills().add(cs);
-    }
-
-    public void removeSkill(Skill skill) {
-        if (skill == null) return;
-        candidateSkills.removeIf(cs -> {
-            if (cs.getSkill().equals(skill)) {
-                skill.getCandidateSkills().remove(cs);
-                cs.setCandidate(null);
-                cs.setSkill(null);
-                return true;
-            }
-            return false;
-        });
-    }
 }
